339d37f9bfe25a6f7adbe542bed0dff04070af02
smilekaka <jinchunliang1988@163.com>
2019-10-05 22:14:45 +0800

    添加了该类所描述的信息注释

--- src/main/java/enumerated/ConstantSpecificMethod.java
+++ src/main/java/enumerated/ConstantSpecificMethod.java
@@ -4,6 +4,10 @@ package enumerated; /* Added by Eclipse.py */
 import java.text.DateFormat;
 import java.util.Date;
 
+/**
+ * enum特性：允许程序员为enum实例编写方法，从而为每个enum实例赋予各自不同的行为
+ * 需要先为enum定义一个或多个abstract方法
+ */
 public enum ConstantSpecificMethod {
     DATE_TIME {
         @Override
