2846444dcf229f03d02f909584893e78cc635b51
smilekaka <jinchunliang1988@163.com>
2019-10-06 09:21:36 +0800

    随机生成enum实例的工具类

--- src/main/java/net/mindview/util/Enums.java
+++ src/main/java/net/mindview/util/Enums.java
@@ -1,13 +1,19 @@
 //: net/mindview/util/Enums.java
 package net.mindview.util;
-import java.util.*;
 
+import java.util.Random;
+
+/**
+ * 根据enum的Class对象随机生成该enum下的任意enum实例
+ */
 public class Enums {
-  private static Random rand = new Random(47);
-  public static <T extends Enum<T>> T random(Class<T> ec) {
-    return random(ec.getEnumConstants());
-  }
-  public static <T> T random(T[] values) {
-    return values[rand.nextInt(values.length)];
-  }
+    private static Random rand = new Random(47);
+
+    public static <T extends Enum<T>> T random(Class<T> ec) {
+        return random(ec.getEnumConstants());
+    }
+
+    public static <T> T random(T[] values) {
+        return values[rand.nextInt(values.length)];
+    }
 } ///:~
